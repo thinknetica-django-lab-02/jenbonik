54f1a2b8d0cd8311a0f064ba6493c1b50584fc5f
Boico Evghenii <gjenbonik@gmail.com>
2021-03-23 00:05:38 +0300

    Create simple index and flatpages template

--- luckydeal/luckydeal/settings.py
+++ luckydeal/luckydeal/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'main'
 ]
 
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# For django.contrib.sites
+SITE_ID = 1
