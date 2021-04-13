edbb9aa8d86cf1864d7b736061f8554059331600
Boico Evghenii <gjenbonik@gmail.com>
2021-04-13 23:05:44 +0300

    Добавление в группу по умолчанию

--- luckydeal/main/models.py
+++ luckydeal/main/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -140,6 +140,7 @@ def create_user_profile(sender, instance, created, **kwargs):
     """ Сохранение профиля пользователя """
     if created:
         UserProfile.objects.create(user = instance)
+        instance.groups.add(Group.objects.get(name='default_group'))
     else:
         instance.userprofile.save()
     
