2b40bb6086894130fb6618ccdadf0eb7627e377e
Boico Evghenii <gjenbonik@gmail.com>
2021-04-18 16:03:24 +0300

    Отправка оповещений по расписанию

--- luckydeal/main/apps.py
+++ luckydeal/main/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        from .sheduler import sheduler_start
+        sheduler_start()
