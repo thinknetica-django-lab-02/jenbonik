ded454d1eafa4f7be67305685cd853c32bca7bbc
Boico Evghenii <gjenbonik@gmail.com>
2021-04-21 21:12:56 +0300

    Отключение старого расписания

--- luckydeal/main/apps.py
+++ luckydeal/main/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     name = 'main'
     verbose_name = 'Основное приложение'
-
-    def ready(self):
-        from .sheduler import sheduler_start
-        sheduler_start()
